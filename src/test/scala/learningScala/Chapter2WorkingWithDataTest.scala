e04c524998421be078e5639fa65719a2c5ca24e0
stefan521 <radu11.sm@gmail.com>
2020-06-02 21:30:41 +0100

    Converted chapter 2 to FlatSpec.

--- src/test/scala/learningScala/Chapter2WorkingWithDataTest.scala
+++ src/test/scala/learningScala/Chapter2WorkingWithDataTest.scala
@@ -1,12 +1,12 @@
 package learningScala
 import Chapter2WorkingWithData._
 import org.scalactic.{Equality, TolerantNumerics}
-import org.scalatest.FunSpec
+import org.scalatest.{FlatSpec, Matchers}
 
-class Chapter2WorkingWithDataTest extends FunSpec {
+class Chapter2WorkingWithDataTest extends FlatSpec with Matchers {
   implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.001)
 
-  it("formats a string with the given value") {
+  it should "formats a string with the given value" in {
     assert(payMeBack(12) == "You owe me $12.0")
     assert(payMeBack(0) == "You don't owe me anything.")
     assert(payMeBack(-22) == "You don't owe me anything.")
@@ -14,7 +14,7 @@ class Chapter2WorkingWithDataTest extends FunSpec {
     assert(payMeBack(5001.22) == "You owe me $5001.22")
   }
 
-  it("find the phone number in an address with regex") {
+  it should "find the phone number in an address with regex" in {
     assert(callFrank("Frank,123 Main,925-555-1943,95122") == "925-555-1943")
   }
 }
